43ce17d4424576d14b1b7f9fea2b239bc6922f39
Julia Bond <julia@yogaed.com>
2015-07-25 19:55:19 -1000

    Added solution from Codewars. Installed dependencies

---
+++ throwingdarts.js
@@ -0,0 +1,29 @@
+module.export = scoreThrows;
+
+function scoreThrows(radiuses){
+  //Return total number of points
+  var score = 0;
+  var isTrue = [];
+
+  //Empty array
+  if (radiuses.length === 0){
+    score = 0;
+  }
+  else{
+    for (var i = 0; i < radiuses.length; i++) {
+      if (radiuses[i] < 5){
+        score += 10;
+        isTrue.push(true);
+      } else if (radiuses[i] <= 10){
+        score += 5;
+        isTrue.push(false);
+      } else {
+        isTrue.push(false);
+      }
+    }
+    if (isTrue.indexOf(false) === -1) {
+      score += 100;
+    }
+  }
+  return score;
+}
